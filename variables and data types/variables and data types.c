4bf43a09170ec525164409c67de9e3e4fe20706c
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-04-20 17:48:50 +0530

    initial commit

--- variables and data types/variables and data types.c
+++ variables and data types/variables and data types.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-void main()
+int main()
     {
-  printf("Hello World");
+    // variables
+    // 1.int 2.float 3.char 4.double
+  printf("Hello World\n");
+    int a =10;  // ---> //int is a data type
+    int b=2;  // ---> data type name float
+    printf("%d",(a+b));
     }
