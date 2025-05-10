303103726a7ee833da40a58b5cf6e294dac9d85c
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-05-10 23:38:56 +0530

    initial commit

---
+++ typecasting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// typecasting syntax
+// (type) value;
+
+// typecasting is used to convert one date type to another data type 
+
+int main(){
+    int a=23;
+    float b=43.3;
+    printf("the value of a is %d and b is %f\n",a,b);
+    
+    // now we use typecasting to convert varible data type 
+    printf("the value of a is %f and b is %d",(float)a,(int) b);
+    
+}
