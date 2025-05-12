b80855175a57ef4e5cf9f282e2a06e3640b8697b
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-05-12 22:23:43 +0530

    initial commit

---
+++ function_in.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+
+    // function
+    /*
+    Function are used to divide a large program into small pieces
+    A function can be called multiple times to provide reusability and modularity to the program
+    also called procedure or subroutine.
+
+
+    advantages of function
+    we can avoid rewriting same logic through function
+    we can divide the work among programmars using function
+    we can easily debug  a program using function 
+
+
+
+    declaration,defination or call 
+    A fucntion is declared to tell a compiler about its existance
+    a function is define to get some task done 
+    a function is called in order to be used
+    
+    
+    */
+
+    printf("hello world");
+}
