841f45665dd18871fbc8b54d7d98c7bf51492259
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-05-16 23:41:45 +0530

    initial commit

---
+++ array_in.c
@@ -0,0 +1,27 @@
+/*
+What is array?
+The collection of similer data types 
+Items are stored at contiguous memory location 
+1-d array is like a list 
+2-d arrat is like a table
+the c language place no limits on the number of dimensions in an array 
+
+Why do we need arrays?
+
+code that use array is sometimes more organized and readable
+
+advantage of array 
+
+It is used to represent multiple data items of same type by using only single name 
+
+*/
+
+#include<stdio.h>
+int main(){
+
+
+
+
+    
+    return 0;
+}
