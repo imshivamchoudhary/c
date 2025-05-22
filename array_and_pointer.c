32f961fdd90cfe317a5da6416582824f8825ccb8
imshivamchoudhary <shivamchoudhary012@gmail.com>
2025-05-22 22:21:02 +0530

    initial commit

---
+++ array_and_pointer.c
@@ -0,0 +1,20 @@
+/*
+Pointers arithmetic
+There are four arithmetic operator that can be used in pointer 
+++
+--
++
+-
+
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int a =34;
+    int* ptra =&a;
+    printf("the adress of a is %d\n",ptra);
+    printf("the adress of a is %d",ptra+1);
+    return 0;
+}
