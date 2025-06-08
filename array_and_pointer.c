993f7410aef2db2677c10795e92f85ee1243c04f
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2025-06-08 21:48:03 +0530

    initial commit

--- array_and_pointer.c
+++ array_and_pointer.c
@@ -27,6 +27,17 @@ int main()
 
     int arr[]={1,2,3,4,4,5,5,6,76};
     printf("Value at position 3 of array is %d\n",arr[3]);
+    printf("The address of first element of the array is %d \n",&arr[0]);
+    printf("The address of first element of the array is %d \n",arr);
+    printf("The address of second element of the array is %d \n",&arr[1]);
+    printf("The address of second element of the array is %d \n",arr+1);
+
+ printf("\n");
+ 
+    printf("The value at address of first element of the array is %d \n",*(&arr[0]));
+    printf("The value at address of first element of the array is %d \n",*(arr));
+    printf("The value at address of second element of the array is %d \n",*(&arr[1]));
+    printf("The value at address of second element of the array is %d \n",*(arr+1));
 
     return 0;
 }
