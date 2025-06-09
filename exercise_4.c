12969e5dd67955d944ba162d75af96deffc235d9
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2025-06-09 23:09:56 +0530

    initial commit

---
+++ exercise_4.c
@@ -0,0 +1,14 @@
+// take input from the user and ask user to choose 0 for star pattern and 1 for reversed triangular star pattern
+
+
+// *
+// **
+// ***
+// **** triangular star patter
+
+
+// ***** reversed triangular star patterm
+// ****
+// ***
+// **
+// *
