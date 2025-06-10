2a047c4bc5d70cc69d41920e84ab00b870947de5
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2025-06-10 23:08:12 +0530

    initial commit

--- exercise_4.c
+++ exercise_4.c
@@ -12,3 +12,25 @@
 // ***
 // **
 // *
+
+
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number you want to print star patter \n");
+    scanf("%d\n",&n);
+    printf("it is triangualr star pattern\n");
+    for (int i = 0; i <= n; i++)
+    {for (int j = 0; j <=i; j++)
+    {
+        printf("*");
+    }
+    
+        
+        printf("\n");
+    }
+    
+     return 0;
+}
