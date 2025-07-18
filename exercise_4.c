add0749440aab1ec8a445a14d84c671e4094626c
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2025-07-18 23:57:04 +0530

    initial commit

--- exercise_4.c
+++ exercise_4.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 // take input from the user and ask user to choose 0 for star pattern and 1 for reversed triangular star pattern
 
 
@@ -6,22 +7,8 @@
 // ***
 // **** triangular star patter
 
-
-// ***** reversed triangular star patterm
-// ****
-// ***
-// **
-// *
-
-
-
-#include <stdio.h>
-int main()
-{
-    int n;
-    printf("Enter the number you want to print star patter \n");
-    scanf("%d\n",&n);
-    printf("it is triangualr star pattern\n");
+void triangulastar(int n){
+      printf("it is triangualr star pattern\n");
     for (int i = 0; i <= n; i++)
     {for (int j = 0; j <=i; j++)
     {
@@ -31,6 +18,37 @@ int main()
         
         printf("\n");
     }
+    printf("\n");
+    printf("\n");
+}
+
+// ***** reversed triangular star patterm
+// ****
+// ***
+// **
+// *
+
+void reversestar(int n){
+    for (int i = 0; i <= n; i++)
+    {for (int j = 0; j <=n-i-1; j++)
+    {
+        printf("*");
+    }
     
+        
+        printf("\n");
+    }
+    
+}
+
+
+
+int main()
+{
+    int n;
+    printf("Enter the number you want to print star patter \n");
+    scanf("%d\n",&n);
+   triangulastar(n);
+    reversestar(n);   
      return 0;
 }
